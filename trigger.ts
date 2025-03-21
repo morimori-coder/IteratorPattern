09addd710f958e4b87b75df4f666765d7554235a
Ryo Moriya <ryo.o.3031@gmail.com>
2025-03-22 06:28:29 +0900

    [fix]importにファイル拡張子.jsを追加

---
+++ trigger.ts
@@ -0,0 +1,13 @@
+import { Book } from "./Book.js";
+import { BookShelf } from "./BookShelf.js";
+// import {Iterator} from "./Iterator";
+
+const bookshelf:BookShelf = new BookShelf(4);
+bookshelf.appendBook(new Book("Around the World in 80 Days"));
+bookshelf.appendBook(new Book("Bible"));
+bookshelf.appendBook(new Book("Cinderella"));
+bookshelf.appendBook(new Book("Daddy-Long-Legs"));
+
+for (const book of bookshelf) {
+    console.log(book.getName());
+}
